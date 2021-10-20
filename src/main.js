0c5d9049193899713d20d8a45c4e225dccc100fd
dzr975336710 <975336710@qq.com>
2021-10-20 15:53:31 +0800

    UPDATE: global element-plus plugin

--- src/main.js
+++ src/main.js
@@ -4,14 +4,9 @@ import router from './router';
 import store from './store';
 // element scss样式实际由vite.config.js按需引入(styleImport)
 import 'element-plus/packages/theme-chalk/src/base.scss';
-import ElComponents from './plugins/element';
+import { useElementPlus } from './plugins/element';
 
 const app = createApp(App);
-ElComponents.forEach(component => {
-  app.component(component.name, component);
-});
-// 设置全局默认样式
-// const ElementOption = { size: 'small', zIndex: 500 }
-// app.config.globalProperties.$ELEMENT = ElementOption
 
+useElementPlus(app);
 app.use(store).use(router).mount('#app');
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -5,7 +5,28 @@ import {
   ElButton,
   ElForm,
   ElFormItem,
+  ElInput,
+  ElLoading,
+  ElMessage,
 } from 'element-plus';
 
-const components = [ElButton, ElForm, ElFormItem];
-export default components;
+const components = [
+  ElButton,
+  ElForm,
+  ElFormItem,
+  ElInput
+];
+
+const option = { size: 'small', zIndex: 3000 };
+
+export const useElementPlus = (app) => {
+  // element全局配置
+  app.config.globalProperties.$ELEMENT = option;
+  // 组件注册
+  components.forEach((component) => {
+    app.component(component.name, component);
+  });
+  // 插件注册 https://element-plus.gitee.io/zh-CN/component/loading.html
+  app.config.globalProperties.$message = ElMessage;
+  app.config.globalProperties.$loading = ElLoading;
+};
