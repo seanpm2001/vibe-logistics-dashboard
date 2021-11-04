481b1d39d0e67deb869f38c37f8944bf6a20ef2e
dzr975336710 <975336710@qq.com>
2021-11-04 17:56:02 +0800

    UPDATE: mock

--- src/server/mock-prod-server.js
+++ src/server/mock-prod-server.js
@@ -1,8 +1,16 @@
 import { createProdMockServer } from "vite-plugin-mock/es/createProdMockServer";
 import testMock from "./mock/test";
+// import userMock from "./mock/user";
+import roleMock from "./mock/role";
 
-export const mockModules = [...testMock];
+export const mockModules = [
+  ...testMock,
+  // ...userMock,
+  ...roleMock
+];
 
 export function setupProdMockServer() {
+  console.log('mockModules: ', mockModules);
+
   createProdMockServer(mockModules);
 }
