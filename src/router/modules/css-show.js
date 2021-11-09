a1e5a124a3c1a492f1d0d6c9e21f16435ece3c7a
dzr975336710 <975336710@qq.com>
2021-11-09 17:40:24 +0800

    UPDATE: path

--- src/router/modules/css-show.js
+++ src/router/modules/css-show.js
@@ -2,34 +2,53 @@ import Layout from '/@/layout/index.vue';
 
 export const cssShowRouter = [
   {
-    path: '/css',
-    component: Layout,
-    redirect: '/css/eye-ball',
-    name: 'CssShow',
-    meta: {
-      title: 'CssShow',
-      icon: 'component'
-    },
-    children: [
-      {
-        path: 'eye-ball',
-        component: () => import('/@/views/cssShow/eye.vue'),
-        name: 'EyeBall',
-        meta: { title: 'Eye Ball' }
-      },
-      {
-        path: 'hover-card',
-        component: () => import('/@/views/cssShow/hover-card.vue'),
-        name: 'HoverCard',
-        meta: { title: 'Hover Card' }
-      },
-      {
-        path: 'starry-sky',
-        component: () => import('/@/views/cssShow/starry-sky.vue'),
-        target: true,
-        name: 'StarrySky',
-        meta: { title: 'Starry Sky' }
-      },
-    ]
-  }
+    path: '/eye-ball',
+    component: () => import('/@/views/cssShow/eye.vue'),
+    name: 'EyeBall',
+    meta: { title: 'Eye Ball' }
+  },
+  {
+    path: '/hover-card',
+    component: () => import('/@/views/cssShow/hover-card.vue'),
+    name: 'HoverCard',
+    meta: { title: 'Hover Card' }
+  },
+  {
+    path: '/starry-sky',
+    component: () => import('/@/views/cssShow/starry-sky.vue'),
+    target: true,
+    name: 'StarrySky',
+    meta: { title: 'Starry Sky' }
+  },
+  // {
+  //   path: '/css',
+  //   component: Layout,
+  //   redirect: '/css/eye-ball',
+  //   name: 'CssShow',
+  //   meta: {
+  //     title: 'CssShow',
+  //     icon: 'component'
+  //   },
+  //   children: [
+  //     {
+  //       path: 'eye-ball',
+  //       component: () => import('/@/views/cssShow/eye.vue'),
+  //       name: 'EyeBall',
+  //       meta: { title: 'Eye Ball' }
+  //     },
+  //     {
+  //       path: 'hover-card',
+  //       component: () => import('/@/views/cssShow/hover-card.vue'),
+  //       name: 'HoverCard',
+  //       meta: { title: 'Hover Card' }
+  //     },
+  //     {
+  //       path: 'starry-sky',
+  //       component: () => import('/@/views/cssShow/starry-sky.vue'),
+  //       target: true,
+  //       name: 'StarrySky',
+  //       meta: { title: 'Starry Sky' }
+  //     },
+  //   ]
+  // }
 ];
