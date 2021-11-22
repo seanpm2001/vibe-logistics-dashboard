aedb051e942ccd1781e265951492348b474e2099
dzr975336710 <975336710@qq.com>
2021-11-22 13:23:48 +0800

    UPDATE: css hover page

--- src/views/cssShow/Hover-card.vue
+++ src/views/cssShow/Hover-card.vue
@@ -1,30 +1,47 @@
 <template>
-  <div class="container">
-    <div class="card">
-      <div class="circle">
-        <h2>01</h2>
+  <div class="page">
+    <div class="container">
+      <div class="card">
+        <div class="circle">
+          <h2>01</h2>
+        </div>
+        <div class="content">
+          <p>
+            With Vibe’s interactive smart boards and collaboration software, teams can innovate, ideate, and collaborate in real-time from anywhere in the world.
+          </p>
+          <a href="/#/hover-card">Learn More</a>
+        </div>
       </div>
-      <div class="content">
-        <p>
-          With Vibe’s interactive smart boards and collaboration software, teams can innovate, ideate, and collaborate in real-time from anywhere in the world.
-        </p>
-        <a href="/#/hover-card">Learn More</a>
+      <div class="card">
+        <div class="circle">
+          <h2>02</h2>
+        </div>
+        <div class="content">
+          <p>
+            With Vibe’s interactive smart boards and collaboration software, teams can innovate, ideate, and collaborate in real-time from anywhere in the world.
+          </p>
+          <a href="/#/hover-card">Learn More</a>
+        </div>
+      </div>
+      <div class="card">
+        <div class="circle">
+          <h2>03</h2>
+        </div>
+        <div class="content">
+          <p>
+            With Vibe’s interactive smart boards and collaboration software, teams can innovate, ideate, and collaborate in real-time from anywhere in the world.
+          </p>
+          <a href="/#/hover-card">Learn More</a>
+        </div>
       </div>
     </div>
-    <div class="card">
+    <div class="card2">
       <div class="circle">
-        <h2>02</h2>
+        <h2>04</h2>
       </div>
-      <div class="content">
-        <p>
-          With Vibe’s interactive smart boards and collaboration software, teams can innovate, ideate, and collaborate in real-time from anywhere in the world.
-        </p>
-        <a href="/#/hover-card">Learn More</a>
-      </div>
-    </div>
-    <div class="card">
-      <div class="circle">
-        <h2>03</h2>
+      <div class="imgBox">
+        <img :src="buddha1">
+        <img :src="buddha2">
       </div>
       <div class="content">
         <p>
@@ -37,18 +54,16 @@
 </template>
 
 <script setup>
-
+import buddha1 from "/@/assets/img/css-show/buddha1.jpeg";
+import buddha2 from "/@/assets/img/css-show/buddha2.jpeg";
 </script>
 
 <style lang="sass" scoped>
-#app
-  display: flex
-  justify-content: center
-  align-items: center
+.page
   width: 100%
   height: 100%
-  background: liner-gradient(45deg, #ffaf00, #bb02ff)
-  background-attachment: fixed
+  padding-bottom: 20px
+  background: #fff
 
 .container
   position: relative
@@ -107,15 +122,73 @@
       font-family: sans-serif
       font-size: 16px
       color: #666
-    a
-      position: relative
-      display: inline-block
-      margin-top: 20px
-      padding: 10px 20px
-      text-text-decoration: none
-      color: #fff
-      background: #000
-      border-radius: 40px
-      font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif
-
+.content a
+  position: relative
+  display: inline-block
+  margin-top: 20px
+  padding: 10px 20px
+  text-text-decoration: none
+  color: #fff
+  background: #000
+  border-radius: 40px
+  font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif
+  user-select: none
+  
+.card2
+  position: relative
+  width: 300px
+  height: 420px
+  margin: 20px auto
+  background: #fff
+  box-shadow: 0 15px 25px rgba(0, 0, 0, .2)
+  transform-style: preserve-3d
+  transition: 1s
+  .circle, .content a
+    background: #000
+  .circle
+    position: absolute
+    top: 0
+    width: 100%
+    height: 100%
+    background: #000
+    clip-path: circle(180px at center 0)
+    text-align: center
+  h2
+    padding: 30px 0
+    color: #fff
+    font-size: 4.5em
+  .content
+    position: absolute
+    bottom: 10px
+    padding: 20px
+    text-align: center
+    p
+      font-family: sans-serif
+      font-size: 16px
+      color: #666
+  &:hover
+    transform: translateX(50%)
+  &:hover .imgBox
+    transform: rotateY(-180deg) translateZ(1px)
+  .imgBox
+    position: relative
+    width: 100%
+    height: 100%
+    z-index: 1
+    transform-origin: left
+    transform-style: preserve-3d
+    background: #000
+    transition: 1s transform
+    img
+      position: absolute
+      top: 0
+      left: 0
+      width: 100%
+      height: 100%
+      object-fit: cover
+      transform-style: preserve-3d
+      -webkit-backface-visibility: hidden
+      backface-visibility: hidden
+    img:nth-child(2) 
+      transform: rotateY(180deg) translateZ(1px)
 </style>
