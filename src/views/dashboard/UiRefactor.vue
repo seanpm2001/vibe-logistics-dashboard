7f5dc84c422b5a1c0c38053226f93a91b9f1c89e
dzr975336710 <975336710@qq.com>
2021-11-22 16:36:03 +0800

    UPDATE: UI refactor page

--- src/views/dashboard/UiRefactor.vue
+++ src/views/dashboard/UiRefactor.vue
@@ -3,16 +3,59 @@
     <h>Ui Refactor Page</h>
     <div class="content">
       <p>
-        You can click on Css Show or the link with 'Css' in the title to see different styling techniques to get inspiration for UI refactoring.
+        You can click link under Css Show or the link with 'Css' in the title to see different styling techniques to get inspiration for UI refactoring.
       </p>
     </div>
-
-
+    <div class="card-container">
+      <el-space wrap>
+        <el-card class="box-card" style="width: 250px">
+          <template #header>
+            <div class="card-header">
+              <span>Bilibili</span>
+            </div>
+          </template>
+          <div v-for="(item, index) in iframeUrlArr" :key="index" class="text item">
+            <el-link @click="openDialog(index)">{{ item.name }}</el-link>
+          </div>
+        </el-card>
+      </el-space>
+    </div>
+    
+    <el-dialog
+      v-model="centerDialogVisible"
+      title="Css Dialog"
+      top="32px"
+      width="80%"
+      destroy-on-close
+      center
+    >
+      <iframe ref="iframe" class="ui-iframe" :src="iframeUrl"></iframe>
+    </el-dialog>
   </div>
 </template>
 
 <script setup>
+// import DialogIframe from "/@/components/DialogIframe.vue";
+import { ref } from "vue";
 
+const centerDialogVisible = ref(false);
+const iframeUrl = ref('');
+const iframeUrlArr = [
+  {
+    name: 'Earth Transition',
+    url: 'https://www.bilibili.com/video/BV1UF411e744?spm_id_from=333.999.0.0'
+  },
+  {
+    name: 'Parallax Demo',
+    url: 'https://www.bilibili.com/video/BV1aU4y1g7oZ?spm_id_from=333.999.0.0'
+  }
+];
+
+const openDialog = idx => {
+  console.log('idx: ', idx);
+  iframeUrl.value = iframeUrlArr[idx].url;
+  centerDialogVisible.value = true;
+};
 </script>
 
 <style lang="sass" scoped>
@@ -22,8 +65,19 @@
   background-color: rgb(240, 242, 245)
 
 h
-  font-size: 14px
+  display: block
+  margin-bottom: 1rem
+  font-size: 32px
   font-weight: 700
   color: #333
-  display: block
+
+.card-container
+  margin-top: 32px
+  .el-card .item:not(:last-child)
+    margin-bottom: 16px
+  &
+    
+.ui-iframe
+  width: 100%
+  height: 80vh
 </style>
