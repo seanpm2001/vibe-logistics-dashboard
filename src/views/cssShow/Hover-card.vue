f8b93aa5cc17acd02e35c54425946a2712c5ae9a
dzr975336710 <975336710@qq.com>
2021-11-27 21:16:05 +0800

    UPDATE: hover card

--- src/views/cssShow/Hover-card.vue
+++ src/views/cssShow/Hover-card.vue
@@ -35,27 +35,59 @@
         </div>
       </div>
     </div>
-    <div class="card2">
-      <div class="circle">
-        <h2>04</h2>
+    <div class="f-row row-center">
+      <div class="card3">
+        <div class="content">
+          <h2>04</h2>
+          <h3>Card Four</h3>
+          <p>
+            With Vibe’s interactive smart boards and collaboration software, teams can innovate, ideate, and collaborate in real-time from anywhere in the world.
+          </p>
+          <a href="/#/hover-card">Learn More</a>
+        </div>
       </div>
-      <div class="imgBox">
-        <img :src="buddha1">
-        <img :src="buddha2">
-      </div>
-      <div class="content">
-        <p>
-          With Vibe’s interactive smart boards and collaboration software, teams can innovate, ideate, and collaborate in real-time from anywhere in the world.
-        </p>
-        <a href="/#/hover-card">Learn More</a>
+      <div class="card3">
+        <div class="content">
+          <h2>05</h2>
+          <h3>Card Six</h3>
+          <p>
+            With Vibe’s interactive smart boards and collaboration software, teams can innovate, ideate, and collaborate in real-time from anywhere in the world.
+          </p>
+          <a href="/#/hover-card">Learn More</a>
+        </div>
       </div>
     </div>
+    <div class="card2">
+        <div class="circle">
+          <h2>06</h2>
+        </div>
+        <div class="imgBox">
+          <img :src="buddha1">
+          <img :src="buddha2">
+        </div>
+        <div class="content">
+          <p>
+            With Vibe’s interactive smart boards and collaboration software, teams can innovate, ideate, and collaborate in real-time from anywhere in the world.
+          </p>
+          <a href="/#/hover-card">Learn More</a>
+        </div>
+      </div>
   </div>
 </template>
 
 <script setup>
+import { onMounted } from 'vue';
 import buddha1 from "/@/assets/img/css-show/buddha1.jpeg";
 import buddha2 from "/@/assets/img/css-show/buddha2.jpeg";
+import '/@/assets/js/vanilla-tilt.min';
+onMounted(() => {
+  const card3 = document.querySelectorAll(".card3");
+  // eslint-disable-next-line no-undef
+  VanillaTilt.init(card3);
+  // eslint-disable-next-line no-undef
+  // card3.addEventListener("tiltChange", callback);
+});
+
 </script>
 
 <style lang="sass" scoped>
@@ -63,8 +95,8 @@ import buddha2 from "/@/assets/img/css-show/buddha2.jpeg";
   width: 100%
   height: 100%
   padding-bottom: 20px
-  background: #fff
-
+  background: #161623
+  
 .container
   position: relative
   display: flex
@@ -144,13 +176,13 @@ import buddha2 from "/@/assets/img/css-show/buddha2.jpeg";
   transform-style: preserve-3d
   transition: .7s
   .circle, .content a
-    background: #000
+    background: blue
   .circle
     position: absolute
     top: 0
     width: 100%
     height: 100%
-    background: #000
+    background: blue
     clip-path: circle(180px at center 0)
     text-align: center
   h2
@@ -191,4 +223,56 @@ import buddha2 from "/@/assets/img/css-show/buddha2.jpeg";
       backface-visibility: hidden
     img:nth-child(2) 
       transform: rotateY(180deg) translateZ(1px)
+
+.card3
+  position: relative
+  overflow: hidden
+  display: flex
+  justify-content: center
+  align-items: center
+  width: 280px
+  height: 400px
+  margin: 30px
+  box-shadow: 20px 20px 50px rgba(0, 0, 0, .5)
+  border-radius: 15px
+  background: rgba(255, 255, 255, .1)
+  border-top: 1px solid rgba(255, 255, 255, .5)
+  border-left: 1px solid rgba(255, 255, 255, .5)
+  backdrop-filter: blur(5px)
+  &:hover .content
+    transform: translateY(0)
+    opacity: 1
+  .content
+    padding: 20px
+    text-align: center
+    transform: translateY(100px)
+    opacity: 0
+    transition: .5s
+    h2
+      position: absolute
+      top: -100px
+      right: 20px
+      font-size: 8em
+      color: rgba(255, 255, 255, .5)
+      pointer-events: none
+    h3
+      font-size: 1.8em
+      color: #fff
+      z-index: 1
+    p
+      margin-top: 8px
+      font-size: 1em
+      color: #fff
+      font-weight: 300
+    a
+      position: relative
+      display: inline-block
+      padding: 8px 20px
+      margin-top: 15px
+      background: #fff
+      font-weight: 500
+      color: #000
+      text-decoration: none
+      border-radius: 20px
+
 </style>
