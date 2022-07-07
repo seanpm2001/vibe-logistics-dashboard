aa75e73ab492acad1d8aa9c5566d4507b450e139
dzr975336710 <975336710@qq.com>
2022-07-07 16:24:07 +0800

    FIX: get customerId

--- src/views/logistic/OrderList/Index.vue
+++ src/views/logistic/OrderList/Index.vue
@@ -244,7 +244,7 @@ function removeEmptyProducts (taskItem) {
     delete taskItem['products'];
 }
 
-const getCustomerId = () => Number(Object.keys(props.warehouseEnum).find(key => props.warehouseEnum[key] === 'Customer'));
+const getCustomerId = () => Number(Object.keys(warehouseEnum.value).find(key => warehouseEnum.value[key] === 'Customer'));
 
 async function submitInitTaskItem (products, assignedData, orderId) {
   const { sourceWHId, carrier, transportMode } = assignedData;
@@ -254,7 +254,6 @@ async function submitInitTaskItem (products, assignedData, orderId) {
   task.taskType = 'FULFILLMENT';
   task.sourceId = sourceWHId;
   task.targetId = getCustomerId(); // Default Customer
-  console.log('getCustomerId(): ', getCustomerId());
   task.carrier = carrier;
   task.transportMode = transportMode;
   task.products = [];
