aab29e276be89aba37c0408fd1bc212874b4b032
dzr975336710 <975336710@qq.com>
2022-08-04 10:53:24 +0800

    UPDATE: test form

--- src/views/vibe-tool/TestForm.vue
+++ src/views/vibe-tool/TestForm.vue
@@ -1,27 +1,26 @@
 <template>
   <div class="page">
-  {{formData}}
-  <el-form :model="formData">
-    <el-form-item required label="a" error="required" prop="a">
-      <el-input v-model="formData.a"></el-input>
-    </el-form-item>
-    <el-form-item required label="title" error="required" prop="obj.title">
-      <el-input v-model="formData.obj.title"></el-input>
-    </el-form-item>
-    <el-form-item required label="body" error="required" prop="obj.body">
-      <el-input v-model="formData.obj.body"></el-input>
-    </el-form-item>
-    <template v-for="(item, idx) in formData.arr2" :key="idx">
-      <el-form-item required label="name" error="required" :prop="'arr2.' + idx + '.name'">
-        <el-input v-model="item.name"></el-input>
+    {{formData}}
+    <el-form :model="formData">
+      <el-form-item required label="a" error="required" prop="a">
+        <el-input v-model="formData.a"></el-input>
       </el-form-item>
-      <el-form-item required label="age" error="required" :prop="'arr2[' + idx + '].age'">
-        <el-input v-model="item.age"></el-input>
+      <el-form-item required label="title" error="required" prop="obj.title">
+        <el-input v-model="formData.obj.title"></el-input>
       </el-form-item>
-    </template>
-  </el-form>
+      <el-form-item required label="body" error="required" prop="obj.body">
+        <el-input v-model="formData.obj.body"></el-input>
+      </el-form-item>
+      <template v-for="(item, idx) in formData.arr2" :key="idx">
+        <el-form-item required label="name" error="required" :prop="'arr2.' + idx + '.name'">
+          <el-input v-model="item.name"></el-input>
+        </el-form-item>
+        <el-form-item required label="age" error="required" :prop="'arr2[' + idx + '].age'">
+          <el-input v-model="item.age"></el-input>
+        </el-form-item>
+      </template>
+    </el-form>
   </div>
-  
 </template>
 
 <script lang="ts" setup>
