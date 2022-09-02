2f4b4b81c9e25430c8b5793b744d0c29a1fb1a31
dzr975336710 <975336710@qq.com>
2022-09-02 13:16:51 +0800

    FIX: Fulfilled Date

--- src/utils/logistic.ts
+++ src/utils/logistic.ts
@@ -44,7 +44,7 @@ export const formatAssignedOrderItem = orderItem => {
   return orderItem;
 };
 
-export const formatVBDate = (date: string) => date.replace('T', ' ').replace(/\.\d+/, '').replace(/\+.*/, '');
+export const formatVBDate = (date) => String(date).replace('T', ' ').replace(/\.\d+/, '').replace(/\+.*/, '');
 
 export const getTaskOrderIdArr = (taskList: any) :Array<number> => {
   return taskList.map(task => task.orderId);
